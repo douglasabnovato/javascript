38f247aaf2da5429a1097950c4a9812a5b4d045f
@douglasabnovato <douglasabnovato@gmail.com>
2019-11-04 18:15:04 -0300

    criando os todos

--- todos.js
+++ todos.js
@@ -9,6 +9,9 @@ var todos = [
 ];
 
 function renderTodos(){
+
+    listElement.innerHTML = '';
+
     for (todo of todos ){
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
@@ -26,3 +29,5 @@ function addTodo(){
     inputElement.value = '';
     renderTodos();
 }
+
+buttonElement.onclick = addTodo;
