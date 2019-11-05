5d864225389feeb5b612f99559f2fa1a5fbb17e4
@douglasabnovato <douglasabnovato@gmail.com>
2019-11-04 23:57:25 -0300

    desafio 3 - tarefa 1

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-axios.get('https://api.github.com/users/diego3g')
-    .then(function(response) {
-        console.log(response);
+function checaIdade(idade) {
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        return idade >= 18 ? resolve() : reject();
+      }, 2000);
+    });
+  }
+  
+  checaIdade(15)
+    .then(function() {
+      console.log("Maior que 18");
     })
-    .catch(function(error) {
-        console.warn(error);
+    .catch(function() {
+      console.log("Menor que 18");
     });
