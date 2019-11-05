680156db32f55e69812dd4df343df1bb6c0ac1d8
@douglasabnovato <douglasabnovato@gmail.com>
2019-11-04 21:15:38 -0300

    criando os todos

--- todos.js
+++ todos.js
@@ -1,6 +1,6 @@
 var listElement = document.querySelector('#app ul');
-var inputElement = document.querySelector('app input');
-var buttonElement = document.querySelector('app button');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
 
 var todos = [
     'Fazer café',
